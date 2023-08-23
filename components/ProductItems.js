4bd04ea318afa5859698cdf4095cca32d87f0f19
ma-awal <developerawal3@gmail.com>
2023-08-23 14:30:15 +0600

    ProdutItems added

---
+++ components/ProductItems.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function ProductItems({ product }) {
+  return (
+    <div className="card  ">
+      <Link href={`product/${product.slug}`}>
+        <img src={product.image} className="rounded  shadow" />
+      </Link>
+      <div className="flex flex-col justify-between items-center     p-5">
+        <Link href={`product/${product.slug}`}>
+          <h2>{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button mt-1 " type="buttonj">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Layout from '@/components/Layout';
-
+import ProductItems from '@/components/ProductItems';
+import data from '@/utils/data';
 export default function Home() {
   return (
     <>
-      <Layout title={'Home page'}>Home Page</Layout>
+      <Layout title={'Home page'}>
+        <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+          {data.products.map((product) => (
+            <ProductItems product={product} key={product.id} />
+          ))}
+        </div>
+      </Layout>
     </>
   );
 }
