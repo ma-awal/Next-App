29a0300a86a69364866ab9ca625750d70f408139
ma-awal <developerawal3@gmail.com>
2023-08-28 19:04:35 +0600

    save cart items by cookies

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Store } from '@/utils/Store';
 const Layout = ({ title, children }) => {
   const { state } = useContext(Store);
   const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
   return (
     <>
       <Head>
@@ -22,9 +26,9 @@ const Layout = ({ title, children }) => {
             <div>
               <Link href="/cart" className="p-2">
                 Cart
-                {cart.cartItems.length > 0 && (
+                {cartItemsCount > 0 && (
                   <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    {cartItemsCount}
                   </span>
                 )}
               </Link>
